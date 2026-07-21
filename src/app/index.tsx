583017204560c80b31b78852676fc66097b79600
Wellett <ferarka64@gmail.com>
2026-07-21 18:01:24 +0700

    ффффффффф

---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Header } from '../widgets/header/header.tsx';
+import { Hero } from '../widgets/hero/hero.tsx';
+import { Footer } from '../widgets/footer/footer.tsx';
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  );
+};
