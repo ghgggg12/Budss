9347a6bb539bb0feb16efbbe776694b2fb491857
Wellett <ferarka64@gmail.com>
2026-07-22 01:57:31 +0700

    UPD

---
+++ src/widgets/footer/footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './footer.css';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div className="Background" />
+    </footer>
+  );
+};
