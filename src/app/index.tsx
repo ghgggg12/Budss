c92a4bc2db2a8c914c88133412152f07aba2ed16
Wellett <ferarka64@gmail.com>
2026-07-21 18:00:33 +0700

    Delete src directory

--- src/app/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Section } from '../section/ui/section.tsx';
-
-export const App: React.FC = () => {
-  return <Section />;
-};
--- src/shared/ui/types/flex/types.ts
+++
@@ -1,5 +0,0 @@
-export const flexbox = 'display,flex-direction,flex-wrap,flex-flow,justify-content,align-items,align-content,order,flex-grow,flex-shrink,flex-basis,flex,align-self,gap,row-gap,column-gap';
-
-export function props(): string[] {
-  return flexbox.split(',');
-}
