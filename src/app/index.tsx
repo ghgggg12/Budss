c17b6c90625e2e46b1b308f4fdeaa3da073fa555
Wellett <ferarka64@gmail.com>
2026-08-10 21:55:51 +0600

    new sectionnnnnnnnnnnnnnnnnnnnnn
    feat: new section footer

---
+++ src/app/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Header } from '../widgets/Header/index.ts';
+import { Hero } from '../widgets/Hero/index.ts';
+import { AboutSection } from '../widgets/Hero/About/About.tsx';
+import { HowWorks } from '../widgets/Hero/HowWorks/HowWorks.tsx';
+import { Stats } from '../widgets/Hero/Stats/Stats.tsx';
+import { WhyBudss } from '../widgets/Hero/WhyBudss/WhyBudss.tsx';
+import { Brand } from '../widgets/Hero/Brand/Brand.tsx';
+import { Footer } from '../widgets/Footer/index.ts';
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <AboutSection />
+      <HowWorks />
+      <Stats />
+      <WhyBudss />
+      <Brand />
+      <Footer />
+    </>
+  );
+};
---
+++ src/widgets/Hero/HowWorks/HowWorks.tsx
@@ -0,0 +1,42 @@
+import { Flex } from '../../../shared/ui/types/flex/Flex.tsx'
+import Phone from '../../../shared/assets/images/phone.png';
+import Orange from '../../../shared/assets/svg/orange.svg';
+import Purple from '../../../shared/assets/svg/purple.svg';
+import Blue from '../../../shared/assets/svg/blue.svg';
+import Invisible from '../../../shared/assets/svg/invisible.svg'
+import './HowWorks.css';
+
+export const HowWorks = () => (
+    <section className="HowWorksSection">
+        <Flex className="HowWorksSection_Image" justifyContent="center">
+            <img src={Phone} className="Image_Phone" />
+        </Flex>
+
+        <Flex className="HowWorksSection_Text" flexDirection="column" justifyContent="center">
+            <Flex className="Text_row1 row">
+                <div className="marquee marquee_row1">
+                    <img src={Blue} alt="Text" className="Text" />
+                    <img src={Invisible} alt="Text" className="Text" />
+                    <img src={Blue} alt="Text" className="Text" aria-hidden="true" />
+                    <img src={Invisible} alt="Text" className="Text" aria-hidden="true" />
+                </div>
+            </Flex>
+            <Flex className="Text_row2 row">
+                <div className="marquee marquee-reverse">
+                    <img src={Invisible} alt="Text" className="Text" />
+                    <img src={Purple} alt="Text" className="Text" />
+                    <img src={Invisible} alt="Text" className="Text" aria-hidden="true" />
+                    <img src={Purple} alt="Text" className="Text" aria-hidden="true" />
+                </div>
+            </Flex>
+            <Flex className="Text_row3 row">
+                <div className="marquee marquee_row3">
+                    <img src={Orange} alt="Text" className="Text" />
+                    <img src={Invisible} alt="Text" className="Text" />
+                    <img src={Orange} alt="Text" className="Text" aria-hidden="true" />
+                    <img src={Invisible} alt="Text" className="Text" aria-hidden="true" />
+                </div>
+            </Flex>
+        </Flex>
+    </section>
+);
