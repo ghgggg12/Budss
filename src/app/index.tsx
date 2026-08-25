e7a97620978cac0b813685ef09f57ef01813361f
Wellett <ferarka64@gmail.com>
2026-08-25 15:17:06 +0600

    fix and refact

---
+++ src/app/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Header } from '../widgets/Header/index.ts';
+import { Hero } from '../widgets/Hero/index.ts';
+import { AboutSection } from '../widgets/Hero/About/index.ts';
+import { HowWorks } from '../widgets/Hero/HowWorks/index.ts';
+import { Stats } from '../widgets/Hero/Stats/index.ts';
+import { WhyBudss } from '../widgets/Hero/WhyBudss/index.ts';
+import { Brand } from '../widgets/Hero/Brand/index.ts';
+import { Footer } from '../widgets/Footer/index.ts';
+import { Form } from '../widgets/Hero/Form/index.ts';
+import { BackgroundForm } from '../widgets/Hero/Form/codes/background/background.tsx';
+
+export const App: React.FC = () => {
+  return (
+    <>
+      <Header />
+      <Form />
+      <Hero />
+      <AboutSection />
+      <HowWorks />
+      <Stats />
+      <WhyBudss />
+      <Brand />
+      <Footer />
+      <BackgroundForm />
+    </>
+  );
+};
