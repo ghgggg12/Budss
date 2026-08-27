62c8e811ee6a13211e25e91052f64e63eda31d90
Wellett <ferarka64@gmail.com>
2026-08-28 01:39:19 +0700

    Refact and adaptive for mobile - WhyBudss

--- src/widgets/WhyBudss/WhyBudss.tsx
+++ src/widgets/WhyBudss/WhyBudss.tsx
@@ -1,6 +1,7 @@
 import { Flex } from '@shared/ui/types/flex/Flex.tsx';
 import backgroundText from '@shared/assets/svg/backgroundText.svg';
 import Mobile from '@shared/assets/svg/Mobile.svg';
+import MobileAdapt from '@shared/assets/svg/MobileAdapt.svg';
 import Basket1 from '@shared/assets/svg/Basket1.svg';
 import Basket2 from '@shared/assets/svg/Basket2.svg';
 import Basket3 from '@shared/assets/svg/Basket3.svg';
@@ -10,83 +11,89 @@ import './WhyBudss.css';
 
 export const WhyBudss = () => (
     <section className="WhyBudssSection">
-        <Flex className='WhyBudssSection_Header' justifyContent='Center'>Why Budss</Flex>
-        <Flex className='WhyBudssSection_About' height='100px' justifyContent='Center'>We Provide Advanced Payment Solutions<br/>To Grow Your Business</Flex>
+        <Flex className='WhyBudssSection_Header' justifyContent='center'>Why Budss</Flex>
+        <div className='WhyBudssSection_About'>
+            We Provide Advanced Payment Solutions<br/>
+            &nbsp;To Grow Your Business
+        </div>
         <Flex className='WhyBudssSection_backgroundText' justifyContent='center'><img src={backgroundText} alt='background'/></Flex>
 
         <Flex className='WhyBudssSection_Containers' flexDirection='column' alignItems='center'>
-            <Flex className='Containers_Row1' justifyContent='center' flexDirection='row'>
-                <Flex className='Row1_Container1' flexDirection='column'>
-                    <Flex className='Container1_Img' justifyContent='flex-start'>
-                        <img src={Mobile} alt='Mobile'/>
-                    </Flex>
-
-                    <Flex className='Container1_Header' justifyContent='flex-end'>
-                        Turn your<br/>
+            <div className='Containers_Row1'>
+                <div className='Row1_Container1'>
+                    <div className='Container1_Header'>
+                        Turn your&nbsp;<br/>
                         customers into<br/>
                         loyal influencers
-                    </Flex>
+                    </div>
 
-                    <Flex className='Container1_About' justifyContent='flex-end'>
-                        Budss helps businesses harness the<br/>
-                        exponential power of loyal customers<br/>
+                    <div className='Container1_About'>
+                        Budss helps businesses harness the<br/>&nbsp;
+                        exponential power of loyal customers&nbsp;<br/>
                         for acquisition and retention.
-                    </Flex>
-                </Flex>
+                    </div>
 
-                <Flex className='Row1_Container2' flexDirection='column'>
-                    <Flex className='Container2_Header' justifyContent='flex-start'>
-                        Harness the power<br/>
+                    <div className='Container1_Img'>
+                        <img src={Mobile} alt='Mobile'/>
+                    </div>
+                    <div className='Container1_ImgAdapt'>
+                        <img src={MobileAdapt} alt='Mobile'/>
+                    </div>
+                </div>
+
+                <div className='Row1_Container2'>
+                    <div className='Container2_Header'>
+                        Harness the power<br/>&nbsp;
                         of buying in bulk
-                    </Flex>
+                    </div>
 
-                    <Flex className='Container2_About' justifyContent='flex-start'>
-                        Get at least 2X more sales with Budss Buying<br/>
-                        Groups. Your customer base will grow<br/>
-                        exponentially as your customers add their<br/>
+                    <div className='Container2_About'>
+                        Get at least 2X more sales with Budss Buying<br/>&nbsp;
+                        Groups. Your customer base will grow<br/>&nbsp;
+                        exponentially as your customers add their<br/>&nbsp;
                         friends & family to your personalized group.
-                    </Flex>
+                    </div>
                     <Flex className='Container2_Imgs'>
                         <img className='Container2_Basket1' src={Basket1} alt='basket'/>
                         <img className='Container2_Basket2' src={Basket2} alt='basket'/>
                         <img className='Container2_Basket3' src={Basket3} alt='basket'/>
                     </Flex>
-                </Flex>
-            </Flex>
+                </div>
+            </div>
 
-            <Flex className='Containers_Row2' justifyContent='center' flexDirection='row' flexGrow="1">
-                <Flex className='Row2_Container3' flexDirection='column'>
-                    <Flex className='Container3_Header'>
-                        Supercharge<br/>
-                        Loyalty With Instant<br/>
+            <div className='Containers_Row2'>
+                <div className='Row2_Container3'>
+                    <div className='Container3_Header'>
+                        Supercharge<br/>&nbsp;
+                        Loyalty With Instant<br/>&nbsp;
                         Cash Back
-                    </Flex>
-                    <Flex className='Container3_About'>
-                        Budss allows for shoppers to earn<br/>
-                        immediate cash back at their<br/>
+                    </div>
+                    <div className='Container3_About'>
+                        Budss allows for shoppers to earn<br/>&nbsp;
+                        immediate cash back at their&nbsp;<br/>
                         favorite stores.
-                    </Flex>
-                    <Flex className='Container3_Img'>
+                    </div>
+                    <div className='Container3_Img'>
                         <img src={BlueMan} alt='Man'/>
-                    </Flex>
-                </Flex>
+                    </div>
+                </div>
 
-                <Flex className='Row2_Container4' flexDirection='column'>
-                    <Flex className='Container4_Header'>
-                        Seasoned with AI<br/>
+                <div className='Row2_Container4'>
+                    <div className='Container4_Header'>
+                        Seasoned with AI<br/>&nbsp;
                         and Gamification
-                    </Flex>
-                    <Flex className='Container4_About'>
-                        AI programming creates a personalized<br/>
-                        customer journey, offering recommendations<br/>
-                        for their next purchase and optimizing<br/>
+                    </div>
+                    <div className='Container4_About'>
+                        AI programming creates a personalized&nbsp;<br/>
+                        customer journey, offering recommendations<br/>&nbsp;
+                        for their next purchase and optimizing<br/>&nbsp;
                         rewards for after purchase.
-                    </Flex>
-                    <Flex className='Container4_Img' justifyContent='flex-end'>
+                    </div>
+                    <div className='Container4_Img'>
                         <img src={PurpleMan} alt='Man'/>
-                    </Flex>
-                </Flex>
-            </Flex>
+                    </div>
+                </div>
+            </div>
         </Flex>
     </section>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@features': path.resolve(__dirname, './src/features'),
+      '@widgets': path.resolve(__dirname, './src/widgets'),
+      '@': path.resolve(__dirname, './src'),
+    }
+  }
 })
